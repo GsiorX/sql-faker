9a20fabc4d6a045f13c02b7a0baca25e80ee0c83
Jakub Gasiorek <220890@student.pwr.edu.pl>
2021-01-11 14:26:20 +0100

    Implement inmemory queries time measurement

--- sql-faker/time-measure/main.py
+++ sql-faker/time-measure/main.py
@@ -33,6 +33,94 @@ class IndexCache:
         return Index()
 
 
+class Inmemory:
+    def __init__(self, cursor, table_name: str, table_name2: str = None, col: str = None, col2: str = None):
+        self.cursor = cursor
+        self.table_name = table_name
+        self.table_name2 = table_name2
+        self.col = col
+        self.col2 = col2
+
+    def with_inmemory_column(self):
+        cursor = self.cursor
+        table_name = self.table_name
+        col = self.col
+
+        class InMemory:
+            def __enter__(self):
+                cursor.execute("alter table " + table_name + " INMEMORY")
+                cursor.execute("alter table " + table_name + " INMEMORY (" + col + ")")
+
+                cursor.execute("exec DBMS_INMEMORY.POPULATE('C##ZSBD', '" + table_name + "');")
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                cursor.execute("alter table " + table_name + " NO INMEMORY")
+                cursor.execute("alter table " + table_name + " NO INMEMORY (" + col + ")")
+
+        return InMemory()
+
+    def with_inmemory_join_group(self):
+        table_name = self.table_name
+        table_name2 = self.table_name2
+        col = self.col
+        col2 = self.col2
+
+        class InMemoryJoinGroup:
+            def __enter__(self):
+                cursor.execute("ALTER TABLE " + table_name + " INMEMORY")
+                cursor.execute("ALTER TABLE " + table_name2 + " INMEMORY")
+                cursor.execute("CREATE INMEMORY JOIN GROUP " + table_name + col + table_name2 + col2 + " (C##ZSBD." + table_name + "(" + col + "), C##ZSBD." + table_name2 + "(" + col2 + "))")
+
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', table_name])
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', table_name2])
+
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                cursor.execute("DROP INMEMORY JOIN GROUP " + table_name + col + table_name2 + col2)
+                cursor.execute("ALTER TABLE " + table_name + " NO INMEMORY")
+                cursor.execute("ALTER TABLE " + table_name2 + " NO INMEMORY")
+
+        return InMemoryJoinGroup()
+
+    def with_inmemory_za2(self):
+        class InMemoryZa2:
+            def __enter__(self):
+                cursor.execute("ALTER TABLE CREWMEMBER INMEMORY")
+                cursor.execute("ALTER TABLE CREWMEMBER_JOB INMEMORY")
+                cursor.execute("ALTER TABLE JOB INMEMORY")
+                cursor.execute("ALTER TABLE CREWMEMBER_AWARD INMEMORY")
+                cursor.execute("ALTER TABLE AWARD INMEMORY")
+
+                try:
+                    cursor.execute("CREATE INMEMORY JOIN GROUP joingroup1 (C##ZSBD.CREWMEMBER(crewmemberid), C##ZSBD.CREWMEMBER_JOB(crewmembercrewmemberid), C##ZSBD.CREWMEMBER_AWARD(crewmembercrewmemberid))")
+                    cursor.execute("CREATE INMEMORY JOIN GROUP joingroup2 (C##ZSBD.JOB(jobid), C##ZSBD.CREWMEMBER_JOB(jobjobid))")
+                    cursor.execute("CREATE INMEMORY JOIN GROUP joingroup3 (C##ZSBD.AWARD(awardid), C##ZSBD.CREWMEMBER_AWARD(awardawardid))")
+                except:
+                    print(1)
+
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', 'CREWMEMBER'])
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', 'CREWMEMBER_JOB'])
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', 'JOB'])
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', 'CREWMEMBER_AWARD'])
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', 'AWARD'])
+
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                try:
+                    cursor.execute("DROP INMEMORY JOIN GROUP joingroup1")
+                    cursor.execute("DROP INMEMORY JOIN GROUP joingroup2")
+                    cursor.execute("DROP INMEMORY JOIN GROUP joingroup3")
+                except:
+                    print(1)
+                cursor.execute("ALTER TABLE CREWMEMBER NO INMEMORY")
+                cursor.execute("ALTER TABLE CREWMEMBER_JOB NO INMEMORY")
+                cursor.execute("ALTER TABLE JOB NO INMEMORY")
+                cursor.execute("ALTER TABLE CREWMEMBER_AWARD NO INMEMORY")
+                cursor.execute("ALTER TABLE AWARD NO INMEMORY")
+
+        return InMemoryZa2()
+
+
 class CleanCache:
     def __init__(self, cursor):
         self.cursor = cursor
@@ -61,10 +149,15 @@ if __name__ == "__main__":
             za3 = Timer("ZA3")
             for _ in range(10):
                 with CleanCache(sys_cursor).without_cache():
-                    with za1.measure("ZA1"):
-                        queries.za1(1970, 2020)
-                    with za2.measure("ZA2"):
-                        queries.za2('2020-01-01', '2000', 1)
+                    with Inmemory(sys_cursor, "MOVIE", "GENRE_MOVIE", "movieid",
+                                  "moviemovieid").with_inmemory_join_group():
+                        with Inmemory(sys_cursor, "GENRE", "GENRE_MOVIE", "genreid",
+                                      "genregenreid").with_inmemory_join_group():
+                            with za1.measure("ZA1"):
+                                queries.za1(1970, 2020)
+                    with Inmemory(sys_cursor, "fake").with_inmemory_za2():
+                        with za2.measure("ZA2"):
+                            queries.za2('2020-01-01', '2000', 1)
                     with za3.measure("ZA3"):
                         queries.za3('1980-01-01', 50, '2020-01-01', "Buck", 5, 2)
                 connection.rollback()
