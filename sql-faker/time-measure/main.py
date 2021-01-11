f7406d5e1c0ff9046759525a810d09bed46afa5f
Jakub Gasiorek <220890@student.pwr.edu.pl>
2021-01-11 17:28:10 +0100

    Ready script to measure time after introduction of memory stored columns

--- sql-faker/time-measure/main.py
+++ sql-faker/time-measure/main.py
@@ -48,14 +48,14 @@ class Inmemory:
 
         class InMemory:
             def __enter__(self):
-                cursor.execute("alter table " + table_name + " INMEMORY")
-                cursor.execute("alter table " + table_name + " INMEMORY (" + col + ")")
+                cursor.execute("alter table C##ZSBD." + table_name + " INMEMORY")
+                cursor.execute("alter table C##ZSBD." + table_name + " INMEMORY (" + col + ")")
 
-                cursor.execute("exec DBMS_INMEMORY.POPULATE('C##ZSBD', '" + table_name + "');")
+                cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', table_name])
 
             def __exit__(self, exc_type, exc_val, exc_tb):
-                cursor.execute("alter table " + table_name + " NO INMEMORY")
-                cursor.execute("alter table " + table_name + " NO INMEMORY (" + col + ")")
+                cursor.execute("alter table C##ZSBD." + table_name + " NO INMEMORY")
+                cursor.execute("alter table C##ZSBD." + table_name + " NO INMEMORY (" + col + ")")
 
         return InMemory()
 
@@ -67,8 +67,8 @@ class Inmemory:
 
         class InMemoryJoinGroup:
             def __enter__(self):
-                cursor.execute("ALTER TABLE " + table_name + " INMEMORY")
-                cursor.execute("ALTER TABLE " + table_name2 + " INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD." + table_name + " INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD." + table_name2 + " INMEMORY")
                 cursor.execute("CREATE INMEMORY JOIN GROUP " + table_name + col + table_name2 + col2 + " (C##ZSBD." + table_name + "(" + col + "), C##ZSBD." + table_name2 + "(" + col2 + "))")
 
                 cursor.callproc("DBMS_INMEMORY.POPULATE", ['C##ZSBD', table_name])
@@ -77,8 +77,8 @@ class Inmemory:
 
             def __exit__(self, exc_type, exc_val, exc_tb):
                 cursor.execute("DROP INMEMORY JOIN GROUP " + table_name + col + table_name2 + col2)
-                cursor.execute("ALTER TABLE " + table_name + " NO INMEMORY")
-                cursor.execute("ALTER TABLE " + table_name2 + " NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD." + table_name + " NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD." + table_name2 + " NO INMEMORY")
 
         return InMemoryJoinGroup()
 
@@ -112,11 +112,11 @@ class Inmemory:
                     cursor.execute("DROP INMEMORY JOIN GROUP joingroup3")
                 except:
                     print(1)
-                cursor.execute("ALTER TABLE CREWMEMBER NO INMEMORY")
-                cursor.execute("ALTER TABLE CREWMEMBER_JOB NO INMEMORY")
-                cursor.execute("ALTER TABLE JOB NO INMEMORY")
-                cursor.execute("ALTER TABLE CREWMEMBER_AWARD NO INMEMORY")
-                cursor.execute("ALTER TABLE AWARD NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD.CREWMEMBER NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD.CREWMEMBER_JOB NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD.JOB NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD.CREWMEMBER_AWARD NO INMEMORY")
+                cursor.execute("ALTER TABLE C##ZSBD.AWARD NO INMEMORY")
 
         return InMemoryZa2()
 
@@ -158,8 +158,9 @@ if __name__ == "__main__":
                     with Inmemory(sys_cursor, "fake").with_inmemory_za2():
                         with za2.measure("ZA2"):
                             queries.za2('2020-01-01', '2000', 1)
-                    with za3.measure("ZA3"):
-                        queries.za3('1980-01-01', 50, '2020-01-01', "Buck", 5, 2)
+                    with Inmemory(sys_cursor, "MOVIE", col="budget").with_inmemory_column():
+                        with za3.measure("ZA3"):
+                            queries.za3('1980-01-01', 50, '2020-01-01', "Buck", 5, 2)
                 connection.rollback()
 
             zd1 = Timer("ZD1")
@@ -168,14 +169,16 @@ if __name__ == "__main__":
             zd4 = Timer("ZD4")
             for _ in range(3):
                 with CleanCache(sys_cursor).without_cache():
-                    with zd1.measure("ZD1"):
-                        queries.zd1(3, '2020-01-01', 2)
-                    with zd2.measure("ZD2"):
-                        queries.zd2(50, 0.25)
-                    with zd3.measure("ZD3"):
-                        queries.zd3(25)
-                    with zd4.measure("ZD4"):
-                        queries.zd4()
+                    # with zd1.measure("ZD1"):
+                    #     queries.zd1(3, '2020-01-01', 2)
+                    with Inmemory(sys_cursor, "MOVIE", col="Title").with_inmemory_column():
+                        with zd2.measure("ZD2"):
+                            queries.zd2(50, 0.25)
+                    # with zd3.measure("ZD3"):
+                    #     queries.zd3(25)
+                    with Inmemory(sys_cursor, "MOVIE", col="Duration").with_inmemory_column():
+                        with zd4.measure("ZD4"):
+                            queries.zd4()
                 connection.rollback()
 
             with open("time-results.csv", "w", newline="") as csvfile:
@@ -185,7 +188,7 @@ if __name__ == "__main__":
                 writer.writerow([za1.name, za1.sum / 10, za1.max, za1.min, 10])
                 writer.writerow([za2.name, za2.sum / 10, za2.max, za2.min, 10])
                 writer.writerow([za3.name, za3.sum / 10, za3.max, za3.min, 10])
-                writer.writerow([zd1.name, zd1.sum / 3, zd1.max, zd1.min, 3])
+                # writer.writerow([zd1.name, zd1.sum / 3, zd1.max, zd1.min, 3])
                 writer.writerow([zd2.name, zd2.sum / 3, zd2.max, zd2.min, 3])
-                writer.writerow([zd3.name, zd3.sum / 3, zd3.max, zd3.min, 3])
+                # writer.writerow([zd3.name, zd3.sum / 3, zd3.max, zd3.min, 3])
                 writer.writerow([zd4.name, zd4.sum / 3, zd4.max, zd4.min, 3])
